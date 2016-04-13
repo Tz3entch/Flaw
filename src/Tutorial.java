17c67b866b780e6e5da18e80fb76c7c6c5512944
Tz3entch <grayjoy1@gmail.com>
2016-04-13 20:24:07 +0300

    tutorial rebuilded

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -7,17 +7,13 @@ import java.awt.event.MouseListener;
 
 public class Tutorial  extends JFrame implements MouseListener {
 
-    private JLabel example1Label;
-    private JLabel example2Label;
-    private JLabel example3Label;
-    private JButton next;
     private JButton back;
     private JTextArea text1;
-    private JTextArea text2;
-    private JTextArea text3;
-    private ImageIcon example1;
-    private ImageIcon example2;
-    private ImageIcon example3;
+    private JLabel blueLabel;
+    private JLabel greenLabel;
+    private JLabel redLabel;
+    private JLabel yellowLabel;
+    private JLabel orangeLabel;
 
     public static void main(String[] args) {
         new Tutorial(new Point());
@@ -34,60 +30,44 @@ public class Tutorial  extends JFrame implements MouseListener {
         c.fill = GridBagConstraints.HORIZONTAL;
         setLocation(p);
 
-//        label = new JLabel("Tutorial will be added soon!");
-//        c.gridy = 1;
-//        getContentPane().add(label, c);
-
-        text1 = new JTextArea("    Flaw is a logic based figure placement game. Each level consists of blocks that combines"+
-                " 2 or more figures (circle or star) into 1 figure. Correct figures have green color and incorrect have red. The goal of"+
-                " the game is to find all ways to transform all red figures into green figures. Player can do that by"+
-                " changing input figures "+ "on the left side of the screen. ", 3, 10);
+        text1 = new JTextArea("    This game is used to find the test vectors of the complicated circuits with the"+
+                " help of the path sensitization algorithm. This game contains three levels.\n" +
+                " Before going starting, player should know function of the each block. ", 3, 10);
         text1.setPreferredSize(new Dimension(750, 20));
         text1.setLineWrap(true);
         text1.setEditable(false);
         text1.setWrapStyleWord(true);
         text1.setBackground(getBackground());
-        c.insets = new Insets(0, 0, 10, 0);
+        c.insets = new Insets(0, 50, 30, 0);
         c.gridy =1;
         getContentPane().add(text1, c);
 
-        example1 = new ImageIcon(Level1.class.getResource("/images/example1.png"));
-        example1Label = new JLabel(example1);
-        c.insets = new Insets(20, 0, 10, 0);
-        c.gridy = 2;
-        getContentPane().add(example1Label, c);
+        blueLabel = new JLabel("Blue Block: The output is star only when all inputs is star.");
+        blueLabel.setForeground(Color.blue);
+        c.insets = new Insets(0, 300, 20, 300);
+        c.gridy = 3;
+        getContentPane().add(blueLabel, c);
 
-        text3 = new JTextArea("    A cross on block's node means that star will change to circle and circle to star"+
-                " when it pass through this node. Sometimes player can see black figures. Black figures are fixed and"+
-                " player can't change it. Each time when player find a new way to make green all red figures, game saves" +
-                " his inputs in the score screen as new correct combo and he gets a score depends on his previous combos."
-                +" To complete a level, player need to find all possible correct combinations.", 4, 10);
-        text3.setPreferredSize(new Dimension(850, 50));
-        text3.setLineWrap(true);
-        text3.setEditable(false);
-        text3.setWrapStyleWord(true);
-        text3.setBackground(getBackground());
-        c.gridy =5;
-        getContentPane().add(text3, c);
+        greenLabel = new JLabel("Green Block: Output is circle only when all inputs are circle.");
+        greenLabel.setForeground(new Color(20, 128, 20));
+        c.gridy = 4;
+        getContentPane().add(greenLabel, c);
 
-        example3 = new ImageIcon(Level1.class.getResource("/images/example3.png"));
-        example3Label = new JLabel(example3);
+        redLabel = new JLabel("Red Block: The output is circle only when all inputs are star.");
+        redLabel.setForeground(Color.red);
+        c.gridy = 5;
+        getContentPane().add(redLabel, c);
+
+        yellowLabel = new JLabel("Yellow Block: When all inputs are same the output is circle.");
+        yellowLabel.setForeground(new Color(198, 166, 25));
         c.gridy = 6;
-        getContentPane().add(example3Label, c);
+        getContentPane().add(yellowLabel, c);
 
-
-        next = new JButton("Next");
-        next.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Point p = getLocation();
-                new Tutorial2(p);
-                dispose();
-            }
-        });
-        c.insets = new Insets(30, 300, 5, 300);
+        orangeLabel = new JLabel("Orange Block: The output is star only when all inputs are circle.");
+        orangeLabel.setForeground(new Color(198, 95, 34, 254));
         c.gridy = 7;
-        getContentPane().add(next, c);
+        getContentPane().add(orangeLabel, c);
+
 
         back = new JButton("Back");
         back.addActionListener(new ActionListener() {
@@ -98,17 +78,13 @@ public class Tutorial  extends JFrame implements MouseListener {
                 dispose();
             }
         });
-        c.insets = new Insets(0, 300, 10, 300);
+        c.insets = new Insets(10, 300, 10, 300);
         c.gridy = 8;
         getContentPane().add(back, c);
 
         setVisible(true);
 
     }
-
-
-
-
 
 
     public void mouseClicked(MouseEvent me) {
